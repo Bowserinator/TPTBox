7cf4131c65dea5871c71bb105e043a9daf270dc6
Bowserinator <bowserinator@gmail.com>
2024-03-19 14:02:08 -0400

    Embed the font

--- game/src/interface/FontCache.cpp
+++ game/src/interface/FontCache.cpp
@@ -1,12 +1,13 @@
 #include "FontCache.h"
 #include "raylib.h"
+#include "./../resources/dogicapixel.h"
 
 FontCache * FontCache::single = nullptr;
 
 void FontCache::init() {
     if (_init) return;
 
-    main_font = LoadFontEx("resources/dogicapixel.ttf", FONT_SIZE, 0, 250);
+    main_font = LoadFont_Dogicapixel();
 
     _init = true;
 }
