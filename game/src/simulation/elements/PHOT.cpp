ce389e947b5fda3910bfa760e598c4d79128cfe7
Bowserinator <bowserinator@gmail.com>
2024-05-28 16:40:13 -0400

    Normalize photon velocities

--- game/src/simulation/elements/PHOT.cpp
+++ game/src/simulation/elements/PHOT.cpp
@@ -25,33 +25,35 @@ static void onCreate(ON_CREATE_FUNC_ARGS) {
     if (mode != PartCreateMode::BRUSH)
         return;
 
+    constexpr float ISQRT3 = 0.57735027f;
+    constexpr float ISQRT2 = 0.70710678f;
     constexpr float DIRECTIONS[][3] = {
-        {-1, -1, -1},
-        {-1, -1, 0},
-        {-1, -1, 1},
-        {-1, 0, -1},
+        {-ISQRT3, -ISQRT3, -ISQRT3},
+        {-ISQRT2, -ISQRT2, 0},
+        {-ISQRT3, -ISQRT3, ISQRT3},
+        {-ISQRT2, 0, -ISQRT2},
         {-1, 0, 0},
-        {-1, 0, 1},
-        {-1, 1, -1},
-        {-1, 1, 0},
-        {-1, 1, 1},
-        {0, -1, -1},
+        {-ISQRT2, 0, ISQRT2},
+        {-ISQRT3, ISQRT3, -ISQRT3},
+        {-ISQRT2, ISQRT2, 0},
+        {-ISQRT3, ISQRT3, ISQRT3},
+        {0, -ISQRT2, -ISQRT2},
         {0, -1, 0},
-        {0, -1, 1},
+        {0, -ISQRT2, ISQRT2},
         {0, 0, -1},
         {0, 0, 1},
-        {0, 1, -1},
+        {0, ISQRT2, -ISQRT2},
         {0, 1, 0},
-        {0, 1, 1},
-        {1, -1, -1},
-        {1, -1, 0},
-        {1, -1, 1},
-        {1, 0, -1},
+        {0, ISQRT2, ISQRT2},
+        {ISQRT3, -ISQRT3, -ISQRT3},
+        {ISQRT2, -ISQRT2, 0},
+        {ISQRT3, -ISQRT3, ISQRT3},
+        {ISQRT2, 0, -ISQRT2},
         {1, 0, 0},
-        {1, 0, 1},
-        {1, 1, -1},
-        {1, 1, 0},
-        {1, 1, 1}
+        {ISQRT2, 0, ISQRT2},
+        {ISQRT3, ISQRT3, -ISQRT3},
+        {ISQRT2, ISQRT2, 0},
+        {ISQRT3, ISQRT3, ISQRT3}
     };
     const auto DIR = DIRECTIONS[sim.rng().rand() % 26];
     sim.parts[i].vx = DIR[0];
